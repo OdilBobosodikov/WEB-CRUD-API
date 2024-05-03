bb427f30cdc7014672c730d1a936688b619c37b2
Odiljon Bobosodikov <odilbobosodikov2002@gmail.com>
2024-05-03 17:38:03 +0500

    Created needed entity and migration

---
+++ WEB-CRUD-API/Model/Entities/Student.cs
@@ -0,0 +1,12 @@
+﻿namespace WEB_CRUD_API.Model.Entities
+{
+    public class Student
+    {
+        public Guid Id { get; set; }
+        public required string Name { get; set; }
+        public int Age { get; set; }
+        public int Year { get; set; }
+        public char Fraction { get; set; }
+
+    }
+}

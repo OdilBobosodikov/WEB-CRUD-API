ce17c99f906983aaa02f14e41bcd59455e333234
Odiljon Bobosodikov <odilbobosodikov2002@gmail.com>
2024-05-03 17:55:26 +0500

    Deleted default API endpoints and added new controller

---
+++ WEB-CRUD-API/Controllers/StudentController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WEB_CRUD_API.Data;
+
+namespace WEB_CRUD_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        private readonly SchoolDbContext dbContext;
+
+        public StudentController(SchoolDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        [HttpGet]
+        public IActionResult GetAllStudents()
+        {
+            return Ok(dbContext.Students.ToList());
+        }
+    }
+}
